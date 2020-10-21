a95b6c8ea2cbbcee5c0ddd285c945a217411b8aa
GreenCyberNinja <1824@holbertonschool.com>
2020-10-21 20:39:58 +0000

    betty compliant

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,8 @@
 #include "sort.h"
 /**
-*
-*
-*
-*
-*
-*
+* bubble_sort - sorts using the bubble algorithm
+* @array: array to sort
+* @size: size of array
 */
 void bubble_sort(int *array, size_t size)
 {
@@ -14,17 +11,15 @@ void bubble_sort(int *array, size_t size)
 	for (x = 0; x < size - 1; x++)
 		for (y = 0; y < size - x - 1; y++)
 			if (array[y] > array[y + 1])
-              		{
+			{
 				swap(&array[y], &array[y + 1]);
 				print_array(array, size);
 			}
 }
 /**
-*
-*
-*
-*
-*
+* swap - swaps the values
+* @a: to be swapped with b
+* @b: to be swapped with a
 */
 void swap(int *a, int *b)
 {
