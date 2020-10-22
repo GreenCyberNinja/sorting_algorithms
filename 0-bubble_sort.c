6007331195fc5791c319708da001f4309d49d6d7
GreenCyberNinja <1824@holbertonschool.com>
2020-10-22 04:58:36 +0000

    edge case(null)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,6 +8,8 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t x, y;
 
+	if (array == NULL)
+		return;
 	for (x = 0; x < size - 1; x++)
 		for (y = 0; y < size - x - 1; y++)
 			if (array[y] > array[y + 1])
